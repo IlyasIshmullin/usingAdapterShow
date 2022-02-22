fd544912705762f1d222aa6bd516f2d973a685b9
IlyasIshmullin <ilya.ishmullin.24.04.2006@gmail.com>
2022-02-22 19:35:28 +0500

    first

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.strictmode.CleartextNetworkViolation;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
-
+    //kmdslkmksmf
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
